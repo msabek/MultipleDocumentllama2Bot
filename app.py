ddd3ce35485d4cd07cd8c68ef9d6430054290eda
Mohamed sabek <147674690+msabek@users.noreply.github.com>
2023-11-26 14:17:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,8 @@ def main():
     # Initialize session state
     initialize_session_state()
     st.title("Evaluation tool for contracts sustainability ")
-    st.write('This project was Developed by group 1 as showcase for CIV E 602 LEC A2 - Fall 2023 - CONTRACT ADMINISTRATION ')
+    st.write('This Project Was Developed by Group 1 As A Showcase For Civ E 602 Lec A2 - Fall 2023 - Contract Administration ')
+    st.write('You can use this tool to evaluate contracts for sustainability ')
     # Initialize Streamlit
     st.sidebar.title("Please drop your contract documents here ")
     uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
